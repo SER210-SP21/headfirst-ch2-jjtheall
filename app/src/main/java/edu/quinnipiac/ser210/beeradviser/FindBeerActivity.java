62a6922455bc81bdb00e13d3827d74919aa10af9
jjtheall <jtheall@quinnipiac.edu>
2022-01-30 19:19:42 -0500

    added initial button functionality

--- app/src/main/java/edu/quinnipiac/ser210/beeradviser/FindBeerActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/beeradviser/FindBeerActivity.java
@@ -1,8 +1,10 @@
 package edu.quinnipiac.ser210.beeradviser;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class FindBeerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
 
+    }
+
+    //called when button is clicked
+    public void onClickFindBeer(View view){
+        //get a reference to the TextView
+        TextView brands = findViewById(R.id.brands);
+        //get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //display selected item
+        brands.setText(beerType);
+        
     }
 }
